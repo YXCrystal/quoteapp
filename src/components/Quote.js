4e4ab2ef3c4e0c2bf451273e86f5bc115452b3de
YXCRYSTAL <yxcwuu@gmail.com>
2020-07-17 13:59:50 -0400

    Add styling to quote box

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,16 +1,18 @@
 import React from "react";
-import "./Quote.css";
+import "../App.css";
 
 class Quote extends React.Component {
 	render() {
 		return (
 			<div class="quote-box">
-				<p>
-					{" "}
+				<p class="paragraph">
 					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus eius
 					nostrum inventore architecto doloribus aliquid eligendi cumque quibusdam
 					odit sapiente quasi asperiores expedita voluptatibus autem, tenetur vitae
-					rem reiciendis excepturi.{" "}
+					rem reiciendis excepturi.
+				</p>
+				<p class="author">
+					<em> - John Doe </em>
 				</p>
 			</div>
 		);
