d02569c3cd2e60d28139db6278ed50e7eb7efa0e
YXCRYSTAL <yxcwuu@gmail.com>
2020-07-17 15:06:56 -0400

    Add new quote button that generates a new quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -8,14 +8,31 @@ class Quote extends React.Component {
 		this.props.fetchQuote();
 	}
 
+	newQuote = () => {
+		this.props.fetchQuote();
+	};
+
 	render() {
 		if (this.props.quote) {
 			return (
-				<div className="quote-box">
-					<p className="paragraph">{this.props.quote.text}</p>
-					<p className="author">
-						<em> - {this.props.quote.author || "Anonymous"} </em>
-					</p>
+				<div>
+					<div className="quote-box">
+						<p className="paragraph">{this.props.quote.text}</p>
+						<p className="author">
+							<em> - {this.props.quote.author || "Anonymous"} </em>
+						</p>
+					</div>
+					<div className="buttons">
+						<a
+							onClick={() => {
+								this.newQuote();
+							}}
+							className="buttons--new"
+							href="#"
+						>
+							New quote
+						</a>
+					</div>
 				</div>
 			);
 		} else {
